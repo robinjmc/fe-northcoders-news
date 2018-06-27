fb8bcf12d36da41c08fd8566084db97839b9939c
robinjmc <robin.jmc@icloud.com>
2018-06-28 00:26:21 +0100

    background color and resize image of footer

--- src/About.js
+++ src/About.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
       return (
-        <div>
-          <header>
-            <img src="https://northcoders.com/images/logos/learn_to_code_manchester_rw_logo.png" alt="Northcoders"/>
-          </header>
+        <div style={{"backgroundColor": "black"}}>
+            <img 
+            style={{transform: "scale(0.70)"}}src="https://northcoders.com/images/logos/learn_to_code_manchester_rw_logo.png" alt="Northcoders"/>
         </div>
       );
     }
