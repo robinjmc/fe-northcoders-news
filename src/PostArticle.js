c31d23ac082fd8d046cedfbd47733f63e7a8c2ed
robinjmc <robin.jmc@icloud.com>
2018-08-29 16:22:07 +0100

    post article event submit refactor

--- src/PostArticle.js
+++ src/PostArticle.js
@@ -54,30 +54,22 @@ class PostArticle extends Component {
             })
     }
 
-    // componentDidUpdate(prevProps, prevState) {
-    //     if (this.state.articleStatus === 'posted') {
-    //         this.setState({
-    //             articleContent: '',
-    //             articleTitle: '',
-    //             topicChoice: '',
-    //             articleStatus: ''
-    //         })
-    //     }
-    // }
-
     chooseTopic = (topic) => {
         this.setState({
             topicChoice: topic.target.value
         })
     }
 
-    postArticle = (article) => {
-        article.preventDefault();
+    postArticle = (articleTitle, articleContent) => {
         const userId = localStorage.getItem("loggedId");
         const { topicChoice } = this.state;
-        postArticle(topicChoice, article.target.elements['articleTitle'].value, article.target.elements['articleContent'].value, userId)
+        postArticle(topicChoice, articleTitle, articleContent, userId)
             .then(res => {
-                return res.json()
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw res;
+                }
             })
             .then(body => {
                 console.log(typeof body)
@@ -85,16 +77,27 @@ class PostArticle extends Component {
                     articleStatus: 'posted',
                     newArticle: body
                 })
-                // return <Link to={`/articles/${body._id}`}></Link> how do i automatically do to this page?
             })
-            .catch(console.log)
+            .catch(err => {
+                console.log(err)
+                this.setState({
+                    error: true,
+                    loading: false,
+                    errorStatus: err.status,
+                    errorType: err.statusText
+                })
+            })
 
+    }
+
+    submit = (event) => {
+        const { articleTitle, articleContent } = this.state
+        event.preventDefault();
+        this.postArticle(articleTitle, articleContent)
     }
 
     render() {
         const { articleContent, loading, topics, topicChoice, articleTitle, error, errorStatus, errorType, newArticle } = this.state;
-        // const { username } = this.props.match.params;
-        console.log(this.props.match)
         let isContent = articleContent.length;
         let isTopic = topicChoice.length;
         let isTitle = articleTitle.length;
@@ -102,32 +105,29 @@ class PostArticle extends Component {
             <div>
                 {
                     error ? <Error errorStatus={errorStatus} errorType={errorType} /> :
-                    Object.entries(newArticle).length ?
-                    <Link to={`/articles/${newArticle._id}`}>View Article</Link>
-                    :
-                        <div>
-                            <form onSubmit={this.postArticle}>
-                                <h1> post article </h1>
-                                <div>
-                                    {
-                                        loading ? <p>Loading...</p> : topics.map(topic => {
-                                            return (
-                                                <div key={topic._id} >
-                                                    <h1>{topic.title}</h1>
-                                                    <input type="radio" name='whichTopic' onClick={this.chooseTopic} value={topic._id} />
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <textarea placeholder="Title" name="articleTitle" onChange={this.handleTitleChange} value={articleTitle} />
-                                <textarea placeholder="Have your say" name="articleContent" onChange={this.handleContentChange} value={articleContent} />
-                                <button className="btn" type="submit" disabled={isContent === 0 || /\S/.test(articleContent) === false || isTopic === 0 || isTitle === 0}>Post article</button>
-                            </form>
-                            {/* <UserArticles username={username}/> */}
-                        </div>
-                        
-                
+                        Object.entries(newArticle).length ?
+                            <Link to={`/articles/${newArticle._id}`}>View Article</Link>
+                            :
+                            <div>
+                                <form onSubmit={this.submit}>
+                                    <h1> post article </h1>
+                                    <div>
+                                        {
+                                            loading ? <p>Loading...</p> : topics.map(topic => {
+                                                return (
+                                                    <div key={topic._id} >
+                                                        <h1>{topic.title}</h1>
+                                                        <input type="radio" name='whichTopic' onClick={this.chooseTopic} value={topic._id} />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <textarea placeholder="Title" name="articleTitle" onChange={this.handleTitleChange} value={articleTitle} />
+                                    <textarea placeholder="Have your say" name="articleContent" onChange={this.handleContentChange} value={articleContent} />
+                                    <button className="btn" type="submit" disabled={isContent === 0 || /\S/.test(articleContent) === false || isTopic === 0 || isTitle === 0}>Post article</button>
+                                </form>
+                            </div>
                 }
             </div>
         )
